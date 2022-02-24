5374eb98f50f3fd0a2e69a843e7b9645de71e5ab
sani2608 <sanikumarsahani@gmail.com>
2022-02-24 23:16:12 +0530

    challengesection component & test component added

---
+++ src/Components/TestContainer/TestContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TestContainer.css';
+
+export const TestContainer = () => {
+    return (
+        <div className='test-container'>
+            <div className='try-again-container'>
+                <h1>This is a try again container</h1>
+            </div>
+        </div>
+    )
+}
